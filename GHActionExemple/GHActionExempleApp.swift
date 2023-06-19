9ea137f26a391bae77675a5ccc4b0e74c6102d04
nadjemTkt <nadjem@tkt.paris>
2023-06-19 09:44:53 +0200

    Initial Commit

---
+++ GHActionExemple/GHActionExempleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GHActionExempleApp.swift
+//  GHActionExemple
+//
+//  Created by nadjem medjdoub on 19/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct GHActionExempleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
